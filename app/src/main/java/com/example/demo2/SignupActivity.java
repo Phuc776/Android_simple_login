6f760f0b78a94cc968767d30077126670a0c256a
Phuc776 <doxphuc@gmail.com>
2023-09-26 11:43:29 +0700

    init commit

---
+++ app/src/main/java/com/example/demo2/SignupActivity.java
@@ -0,0 +1,30 @@
+package com.example.demo2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupActivity extends AppCompatActivity {
+    private TextView link;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        link =  findViewById(R.id.main_login_link);
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLogin();
+            }
+        });
+    }
+
+    public void openLogin() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
