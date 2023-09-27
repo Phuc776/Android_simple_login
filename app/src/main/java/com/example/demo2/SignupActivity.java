1a8c609b2a2d8bc512aff20d4f09f86e1ac6072a
ThanhAn123456 <vacnguyen16092003@gmail.com>
2023-09-27 09:55:03 +0700

    đổi sang finish()

--- app/src/main/java/com/example/demo2/SignupActivity.java
+++ app/src/main/java/com/example/demo2/SignupActivity.java
@@ -19,6 +19,7 @@ public class SignupActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+
             }
         });
     }
